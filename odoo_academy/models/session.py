b77feb1efbee83fdf397afe5b7eda5623471f614
calviar-odoo <calviarpayall@gmail.com>
2022-02-09 13:56:49 +0000

    [ADD] Functions

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -18,7 +18,7 @@ class Session(models.Model):
     
     instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
     
-    student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+    students_ids = fields.Many2many(comodel_name='res.partner', string='Students')
     
     start_date = fields.Date(string='Start Date',
                             default=fields.Date.today)
