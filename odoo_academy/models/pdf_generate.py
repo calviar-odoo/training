8ee10546d8df7bd5e77059de76a5f24e9bf8bb13
calviar-odoo <calviarpayall@gmail.com>
2022-02-16 15:05:05 +0000

    pdf model and view

--- odoo_academy/models/pdf_generate.py
+++ odoo_academy/models/pdf_generate.py
@@ -3,15 +3,17 @@
 from odoo import models,fields,api
 
 class AccountMovePdf(models.Model):
-    _name = 'account.move.pdf'
-    _description = 'Account Model PDF'
+    #_name = 'account.move.pdf'
+    
+    #_description = 'Account Model PDF'
     
     _inherit = 'account.move'
     
-    pdf_invoice = fields.Binary(string='Factura en PDF')
+    #pdf_invoice = fields.Binary(string='Factura en PDF')
+    pdf_invoice = fields.Binary(string = 'Factura en PDF', required = True, default=lambda self: self.pdf_generator()) # Llamamos a la función pdf_generator
     
-    @api.model # model se utiliza para que tome lo que carga la pagina
-    def _model_pdf_invoice(self):
-        pdf = self.env['report'].sudo().get_pdf([invoice.id], 'account.report_invoice')
-        
-        
+    @api.model
+    def pdf_generator(self):
+        invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice')])
+        for invoice in invoices:
+            self.pdf_invoice = self.env['Factura en PDF']
