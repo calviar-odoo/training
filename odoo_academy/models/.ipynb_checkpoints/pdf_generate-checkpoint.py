ef4cd5a7c1444d29ed678d925d4ab35b6691999d
calviar-odoo <calviarpayall@gmail.com>
2022-02-16 19:57:47 +0000

    jelp

--- odoo_academy/models/.ipynb_checkpoints/pdf_generate-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/pdf_generate-checkpoint.py
@@ -1,19 +1,10 @@
 # -*- coding:utf-8 -*-
-
 from odoo import models,fields,api
-
 class AccountMovePdf(models.Model):
-    #_name = 'account.move.pdf'
-    
-    #_description = 'Account Model PDF'
-    
     _inherit = 'account.move'
-    
-    #pdf_invoice = fields.Binary(string='Factura en PDF')
     pdf_invoice = fields.Binary(string = 'PDF', required = True, default=lambda self: self.pdf_generator()) # Llamamos a la función pdf_generator
-    
     @api.model
     def pdf_generator(self):
         invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice')])
         for invoice in invoices:
-            self.pdf_invoice = self.env['account.move'].sudo().render_qweb_pdf([invoice.id], 'account.report_invoice_document')
+            self.pdf_invoice = self.env.ref('account.action_report_invoice').report_action(self)
